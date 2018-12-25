5dcbe7c8cac0424b98c2b5661a5a204758c43829
Nifemi <deyeminifemi@gmail.com>
2018-12-25 21:52:09 +0100

    Enabled adding messages from front end

--- add_message.php
+++ add_message.php
@@ -5,3 +5,14 @@
  * Date: 12/24/2018
  * Time: 9:44 PM
  */
+session_start();
+$db = mysqli_connect('localhost','root','','sandsesq');
+
+if($_POST) {
+    extract($_POST);
+    $stmt = $db->prepare('INSERT INTO `contact`(`name`, `email`, `phone_number`, `message`) VALUES(?,?,?,?)');
+    $stmt->bind_param('ssis', $name, $email, $number, $message);
+    $stmt->execute();
+    $_SESSION['success_flash'] = 'Success!';
+    header('Location: index.php');
+}
